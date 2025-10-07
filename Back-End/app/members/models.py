da0ad17076b4dc3cc27e5a0eee3dad0ff5e32a53
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-07 16:47:25 +0200

    i add a abstractbaseuser and make_password import and i a add register class and the register.password and register.save method

---
+++ Back-End/app/members/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.hashers import make_password
+
+
+class Register(models.Model, AbstractBaseUser):
+# class to manage application registrations
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+    location = models.CharField(max_length=50)
+    email = models.EmailField(max_length=60, unique=True)
+    # password is already included in AbstractBaseUser
+
+# Convert the password to an encrypted version before saving it to the database.
+Register.password = make_password()
+Register.save()
