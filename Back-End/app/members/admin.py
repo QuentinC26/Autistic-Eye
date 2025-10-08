b6d3b1f6534a648cdc7476bf8c6e8231010080fb
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-09 00:45:36 +0200

    i add four import and the class UserAdmin with form = UserChangeForm

--- Back-End/app/members/admin.py
+++ Back-End/app/members/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from .models import User
+from .forms import UserChangeForm
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+     form = UserChangeForm
