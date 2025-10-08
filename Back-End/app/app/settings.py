bf0891146227289ad9f3c0e4004b7589585bccb1
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-08 14:43:27 +0200

    i add django.contrib.site, rest_framework, dj_rest, allauth and members app at the list of installed apps

--- Back-End/app/app/settings.py
+++ Back-End/app/app/settings.py
@@ -39,6 +39,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',
+    "rest_framework",
+    'rest_framework.authtoken',
+    'dj_rest_auth', 
+    'dj_rest_auth.registration', 
+    'allauth', 
+    'allauth.account',
+    "members"
 ]
 
 MIDDLEWARE = [
