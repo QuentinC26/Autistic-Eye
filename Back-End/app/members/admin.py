900687edb57589741e216b70e4ee88857af40efc
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-09 23:59:07 +0200

    i add a form with fieldsets, list_display, search_field and ordering

--- Back-End/app/members/admin.py
+++ Back-End/app/members/admin.py
@@ -6,4 +6,20 @@ from .forms import UserChangeForm
 
 
 class UserAdmin(BaseUserAdmin):
-     form = UserChangeForm
+    form = UserChangeForm
+    # for modify the user information by an admin
+    fieldsets = (
+        (_('Basics Informations'), {'fields': ('first_name', 'last_name', 'age', 'location')}),
+        (_('Connexion'), {'fields': ('email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    # Defines the columns that appear in the user list in the admin interface
+    list_display = ['first_name', 'last_name', 'age', 'location', 'email', 'is_staff']
+    # search for a user by typing a value in the search bar of the admin panel
+    search_fields = ('first_name', 'last_name', 'age', 'location', 'email')
+    # Defines in what order users are listed in the admin.
+    ordering = ('first_name', )
+
+# This line allows the display of the UserAdmin model in the administration interface
+admin.site.register(User, UserAdmin)
