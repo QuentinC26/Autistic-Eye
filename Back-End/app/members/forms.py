cb028387105b80aaf554838b4b3ac7b0a37d9fd0
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-09 23:43:49 +0200

    i add a field = all and comment

--- Back-End/app/members/forms.py
+++ Back-End/app/members/forms.py
@@ -3,6 +3,9 @@ from django.contrib.auth.forms import UserChangeForm as DjangoUserChangeForm
 from .models import User
 
 class UserChangeForm(DjangoUserChangeForm):
-    # Coming
+    # The meta class allows you to retrieve the User model in model.py
     class Meta:
         model = User
+        # i use all fields of class User in model.py
+        fields = '__all__'
+    
