0623d683d272f818c5b2ebd58cdc68f1abe883d1
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-13 21:26:41 +0200

    i add a profile information

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -5,14 +5,20 @@ function Profile() {
   return (
     <>
       <div>
-        <h1>Autistic Eye</h1>
         <a href="index.html" target="_blank">
           <img src={AutisticEye} className="logo Autistic Eye" alt="Autistic Eye logo" />
         </a>
       </div>
         <p>
         <a href="index.html" target="_blank">
-          <h3>Mon Profil</h3>
+          <h3>Mon Profil :</h3>
+         <br></br>
+          <h5>first_name :</h5>
+          <h5>last_name :</h5>
+          <h5>age :</h5>
+          <h5>location :</h5>
+          <h5>email :</h5>
+          <h5>password :</h5>
         </a>
         </p>
     </>
