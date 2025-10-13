db765641d9ac287a79216f1de8e720f1f67c4abe
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-13 22:38:12 +0200

    i remove one import and one picture

--- front-end/src/pages/Community.jsx
+++ front-end/src/pages/Community.jsx
@@ -1,15 +1,8 @@
-import AutisticEye from '../assets/Autistic_Eye.png'
 import './Home.css'
 
 function Community() {
   return (
     <>
-      <div>
-        <h1>Autistic Eye</h1>
-        <a href="index.html" target="_blank">
-          <img src={AutisticEye} className="logo Autistic Eye" alt="Autistic Eye logo" />
-        </a>
-      </div>
         <p>
         <a href="index.html" target="_blank">
           <h3>Community</h3>
