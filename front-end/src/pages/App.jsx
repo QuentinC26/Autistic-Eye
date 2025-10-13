10e8aad53de046fa6758faa4140c174ffd52f7f6
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-13 22:34:48 +0200

    i change the order of nav, and i add a br and className

--- front-end/src/pages/App.jsx
+++ front-end/src/pages/App.jsx
@@ -2,6 +2,8 @@ import Autistic_Eye from '../assets/Autistic_Eye.png'
 import './App.css'
 // manages navigation between application pages
 import { BrowserRouter as Leader_Road, Routes as Road_Office, Route as Single_Road, Link } from 'react-router-dom';
+// manages Home link
+import Home from './Home.jsx'
 // manages Profile link
 import Profile from './Profile.jsx'; 
 // manages Register_and_login link
@@ -16,36 +18,39 @@ function App() {
     <>
     {/* manages navigation between application pages*/}
     <Leader_Road>
-      <nav>
-      <Link to="/">Home</Link>
-      <Link to="/Profile">Mon Profile</Link>
-      <Link to="/Register_and_login">Register/Login</Link>
-    {/* manages all road */} 
-  <Road_Office>
+      <nav> 
+      <ul className="ul_list">
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/Profile">My Profile</Link></li>
+      <li><Link to="/Register_and_login">Register/Login</Link></li>
+    </ul>
+    <br></br>
+    <a href="index.html" target="_blank" rel="noopener noreferrer">
+        {/* rel = nooperner noneferrer is a security for external links */}
+        <img src={Autistic_Eye} className="logo Autistic Eye" alt="Autistic Eye logo" />
+    </a>
+    <br></br>
+    <ul className="ul_list_2">
+    <li><Link to="/Article">Article</Link></li>
+    <li><Link to="/Community">Community</Link></li>
+    </ul>
+    <br></br>
+    <br></br>
+    <br></br>
+     {/* manages all road */} 
+    <Road_Office>
     {/* manages the Home road */}
     <Single_Road path="/" element={<Home />} />
     {/* manages My profile road */}
     <Single_Road path="/Profile" element={<Profile />} />
     {/* manages register and login road */}
     <Single_Road path="/Register_and_login" element={<Register_and_login />} />
-  </Road_Office>
-  </nav>
-      <div>
-        <a href="index.html" target="_blank" rel="noopener noreferrer">
-          {/* rel = nooperner noneferrer is a security for external links */}
-          <img src={Autistic_Eye} className="logo Autistic Eye" alt="Autistic Eye logo" />
-        </a>
-      </div>
-    <div>
-    <nav>
-    <Road_Office>
     {/* manages the article road */}
     <Single_Road path="/Article" element={<Article />} />
     {/* manages the community road */}
     <Single_Road path="/Community" element={<Community />} />
-  </Road_Office>
+    </Road_Office>
   </nav>
-  </div>
     </Leader_Road> 
     </>
   )
