bb42641e7d7cb6d2b2c32b80122514868a70b5a9
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-13 22:36:50 +0200

    i remove the logo picture and one import

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -1,14 +1,8 @@
-import AutisticEye from '../assets/Autistic_Eye.png'
 import './Home.css'
 
 function Profile() {
   return (
     <>
-      <div>
-        <a href="index.html" target="_blank">
-          <img src={AutisticEye} className="logo Autistic Eye" alt="Autistic Eye logo" />
-        </a>
-      </div>
         <p>
         <a href="index.html" target="_blank">
           <h3>Mon Profil :</h3>
