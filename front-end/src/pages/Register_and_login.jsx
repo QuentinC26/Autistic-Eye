37d2b8ebe0a145ff3237515be7476e9e79f2b5fb
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-14 03:21:12 +0200

    i add one import and multiple useState

--- front-end/src/pages/Register_and_login.jsx
+++ front-end/src/pages/Register_and_login.jsx
@@ -1,6 +1,16 @@
-import './Home.css'
+import './App.css'
+import React, { useState } from 'react';
 
-function Register_and_login() {
+function Register() {
+  {/* Creating non-fixed values ​​from a user */}
+  const [first_name, setFirst_name] = useState('');
+  const [last_name, setLast_name] = useState('');
+  const [age, setAge] = useState('');
+  const [location, setLocation] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
   return (
     <>
         <p>
@@ -10,4 +20,4 @@ function Register_and_login() {
   )
 }
 
-export default Register_and_login
+export default Register
