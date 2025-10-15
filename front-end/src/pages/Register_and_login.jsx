b3acd6bef582a2cc3086a7a10534b5cd4400b4b2
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-15 16:06:17 +0200

    i create a function register, the const handleSubmit and i add the forms in the return

--- front-end/src/pages/Register_and_login.jsx
+++ front-end/src/pages/Register_and_login.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import React, { useState } from 'react';
 
 function Register() {
-  {/* Creating non-fixed values ​​from a user */}
+  // Creating non-fixed values ​from a user
   const [first_name, setFirst_name] = useState('');
   const [last_name, setLast_name] = useState('');
   const [age, setAge] = useState('');
@@ -11,13 +11,97 @@ function Register() {
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
 
+
+  // Function that takes care of sending the registration form data to your backend (Django) and handling the response.
+  const handleSubmit = async (register) => {
+    // This prevents the page from reloading when you submit the form, which is important in React to handle this in JavaScript.
+    register.preventDefault();
+
+    try {
+      setMessage('En cours...');
+      const response = await fetch('http://localhost:8000/members/auth/registration/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password1: password,
+        password2: password,
+        first_name,
+        last_name,
+        age,
+        location
+      })
+      });
+      if (response.ok) {
+      setMessage('Vous êtes inscrit !');
+    } else {
+      const errorData = await response.json();
+      setMessage(`Erreur: ${JSON.stringify(errorData)}`);
+    }
+  } catch (error) {
+    setMessage(`Erreur réseau : ${error.message}`);
+  };
+  }; 
+
   return (
     <>
         <p>
-        REGISTER AND LOGIN
+        {/* <form> is an HTML element used to create a form, i.e. a space where the user can fill in information (such as their name, age, etc.). */}
+        <form onSubmit={handleSubmit}>
+          {/* The <input> tag creates a field where the user can enter information. */}
+          <input
+          type="text"
+          placeholder="first_name"
+          value={first_name}
+          onChange={(register) => setFirst_name(register.target.value)}
+          />
+          <br></br>
+          <input
+          type="text"
+          placeholder="last_name"
+          value={last_name}
+          onChange={(register) => setLast_name(register.target.value)}
+          />
+          <br></br>
+          <input
+          type="number"
+          placeholder="Âge"
+          value={age}
+          onChange={(register) => setAge(Number(register.target.value))}
+          />
+          <br></br>
+          <input
+          type="text"
+          placeholder="location"
+          value={location}
+          onChange={(register) => setLocation(register.target.value)}
+          />
+          <br></br>
+          <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(register) => setEmail(register.target.value)}
+          />
+          <br></br>
+          <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(register) => setPassword(register.target.value)}
+          />
+          <br></br>
+          <br></br>
+          {/* read the message if there is one */}
+          {message && <p>{message}</p>}
+          {/* This button is for valid the registration */}
+          <button type="submit">Register</button>
+        </form>
         </p>
     </>
   )
 }
 
-export default Register
+export default Register;
