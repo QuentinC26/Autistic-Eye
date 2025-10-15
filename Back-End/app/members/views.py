0cbde8454d9216862ad436365bd13c80cb99871a
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-16 00:06:37 +0200

    i add two import and i add a class customregisterview

--- Back-End/app/members/views.py
+++ Back-End/app/members/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from dj_rest_auth.registration.views import RegisterView
+from .serializers import CompleteUserSerializer
 
-# Create your views here.
+class CustomRegisterView(RegisterView):
+    # Determines CompleteUserSerializer as the view used to register.
+    serializer_class = CompleteUserSerializer
