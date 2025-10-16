46d9d30f517b58a3fd72deac0648937d28804565
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-16 13:54:25 +0200

    i add import AuthProviver, auth provider tag  and comment

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './pages/App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'  // adapte ce chemin si besoin
 
+// Renders the entire React app inside the <div id="root"> in the HTML page
 createRoot(document.getElementById('root')).render(
+  // Enables additional checks during development
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
