7809fe89abab5bbe3a94d256b7aa27c34ed6798b
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-16 14:53:07 +0200

    iadd a UseContext, AuthCOntext and Navigate import for use the usecontext, authcontext and the navigate in my code

--- front-end/src/pages/Community.jsx
+++ front-end/src/pages/Community.jsx
@@ -1,14 +1,26 @@
 import './App.css'
+// Access the data of the context you created
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
 
 function Community() {
-  return (
-    <>
-        <p>
-        <a href="index.html" target="_blank">
-          <h3>Community</h3>
-        </a>
-        </p>
-    </>
+   // Get the user from the context
+    const { user } = useContext(AuthContext);
+    return (
+      <>
+      {/* ? = the condition is True */}
+      {/* : = the condition is False */}
+       {user ? (
+          <div>
+            <h3>Page Community</h3>
+          </div>
+          ) : (
+            // Redirects the user to the Register/Login page
+            <Navigate to="/Register_and_login" />
+        )}
+      </>
   )
 }
 
