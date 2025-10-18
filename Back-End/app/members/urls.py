7d794373ca60d8c158b3b325c45b398e638eac83
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-18 18:28:04 +0200

    i replace user by missing password in the comment

--- Back-End/app/members/urls.py
+++ Back-End/app/members/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include
 from .views import CustomRegisterView 
 
 urlpatterns = [
-    # road login/logout/user
+    # road login/logout/missing password
     path('auth/', include('dj_rest_auth.urls')),
     # for use the custom register 
     path('auth/registration/', CustomRegisterView.as_view(), name='custom_register')
