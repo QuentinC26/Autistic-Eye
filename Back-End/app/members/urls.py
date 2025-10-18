e5c8ee2af14fec56fc70181e2de30899f6f5f6fe
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-18 23:59:17 +0200

    i add customloginview path and i change the order of auth/ with this new path

--- Back-End/app/members/urls.py
+++ Back-End/app/members/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from django.conf.urls import include
-from .views import CustomRegisterView 
+from .views import CustomRegisterView
+from .views import VerifyEmailView
+from .views import CustomLoginView
 
 urlpatterns = [
+    # Redefines login route to use custom view which blocks non-email verified users
+    path('auth/login/', CustomLoginView.as_view(), name='custom_login'),
+    # for use the custom register 
+    path('auth/registration/', CustomRegisterView.as_view(), name='custom_register'),
+    # To have user verification by email
+    path('auth/verify-email/', VerifyEmailView.as_view(), name='verify-email'),
     # road login/logout/missing password
     path('auth/', include('dj_rest_auth.urls')),
-    # for use the custom register 
-    path('auth/registration/', CustomRegisterView.as_view(), name='custom_register')
 ]
