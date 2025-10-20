3708938344f9c2aba5d148c6ec4ad9e9073fd1ce
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-20 18:55:56 +0200

    i add multiples variables and effect for have the informations of user and i remove the password field

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -1,12 +1,72 @@
 import './App.css'
 // Access the data of the context you created
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 function Profile() {
-  // Get the user from the context
-  const { user } = useContext(AuthContext);
+  // Get the user, accesToken and loading from the context
+  const { user, accessToken, loading } = useContext(AuthContext);
+  // Will store profile data received from the API
+  const [profile, setProfile] = useState(null);
+  // Will be used to display a message in case of a problem
+  const [error, setError] = useState(null);
+
+  // This code runs after the component is initially rendered.
+    useEffect(() => {
+      // It does nothing if the user or token is not yet available
+      if (!user || !accessToken) {
+        return;
+      }
+
+      console.log("Token utilisé pour fetchProfile :", accessToken);
+
+      const fetchProfile = async () => {
+        try {
+          const response = await fetch("http://localhost:8000/members/profile/", {
+          method: "GET",
+          headers: {
+            Authorization: `Token ${accessToken}`,
+            'Content-Type': 'application/json',
+            },
+          });
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          const data = await response.json();
+          console.log("Profile:", data);
+          setProfile(data);
+        } catch (err) {
+          console.error("Erreur de récupération du profil :", err);
+          setError("Impossible de charger les infos du profil.");
+        }
+      };
+
+      // Call the fetchProfile function
+      fetchProfile();
+      // Restart fetchProfile every time 'user' or 'accessToken' changes
+    }, [user, accessToken]);
+
+    // Prevents components from running too early
+    if (loading) {
+      return <p>Chargement des données utilisateur...</p>;
+    } 
+
+    // Prevent access to the profile if you are not logged in
+    if (!user) {
+      return <Navigate to="/Register_and_login" />;
+    }
+
+    // Inform the user that there has been a problem
+    if (error) { 
+      return <p>{error}</p>;
+    }
+
+    // Waiting state to let the user know the application is working
+    if (!profile) {
+      return <p>Chargement du profil...</p>;
+    }
+
   return (
     <>
     {/* ? = the condition is True */}
@@ -16,12 +76,11 @@ function Profile() {
         <a href="index.html" target="_blank">
           <h3>Mon Profil :</h3>
          <br></br>
-          <h5>first_name :</h5>
-          <h5>last_name :</h5>
-          <h5>age :</h5>
-          <h5>location :</h5>
-          <h5>email :</h5>
-          <h5>password :</h5>
+          <h5>first_name : {profile.first_name}</h5>
+          <h5>last_name : {profile.last_name}</h5>
+          <h5>age : {profile.age}</h5>
+          <h5>location : {profile.location}</h5>
+          <h5>email : {profile.email}</h5>
         </a>
         </div>
         ) : (
