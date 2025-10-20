9e29ead025b181c6c81128d0ef2d53607f367c0f
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-20 04:10:23 +0200

    i add a home page for deconnected user

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -2,20 +2,43 @@ import './App.css'
 // Access the data of the context you created
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { Link, useNavigate } from 'react-router-dom';
 
 function Home() {
   // Get the user from the context
   const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleRegister = () => {
+    // To put the user on the home page
+    navigate('/Register_and_login');
+  };
   return (
     <>
     {/* ? = the condition is True */}
     {/* : = the condition is False */}
      {user ? (
         <div>
-        <h3>Bienvenue sur Autistic Eye</h3>
+        <h3>Bienvenue {user} sur Autistic Eye !!</h3>
         </div>
         ) : (
-         <p>Bienvenue sur Autistic Eye, pour vous inscrire :</p>
+        <div>
+         <h3>Bienvenue sur Autistic Eye !!</h3>
+         <br />
+         <p>Autistic Eye est un réseau social communautaire qui réunit les personnes autistes et leurs proches. Son but est de combattre l'isolement des personnes autistes et de leur permettre de partager leurs expériences.</p>
+         <br />
+         <p>Les membres d'Autistic Eye ont un accès illimité à la partie Communauté, qui leur permet de partager leurs expériences, d'échanger entre eux et d'obtenir de l'aide. Pour vous donner un aperçu de cette section, voici le post le plus récent ci-dessous :</p>
+         <br />
+         <p>POST LE PLUS RECENT</p>
+         <br />
+         <p>L'autre partie de l'application est la section Article. Dans cette section, les membres d'Autistic Eye ont accès à toutes les ressources externes qui les concernent, comme les démarches administratives, les associations, et bien plus encore.</p>
+         <br />
+         <p>ARTICLE LE PLUS RECENT</p>
+         <br />
+         <p>Viens nous rejoindre en cliquant sur le boutton ci-dessous :</p>
+         <br />
+         <button onClick={handleRegister}>Inscription</button>
+        </div>
       )}
     </>
   )
