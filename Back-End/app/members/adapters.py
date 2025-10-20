1656283d56bddcfa03851fe4e75ad6c52e2d0ae2
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-20 02:50:48 +0200

    i add old content of frontendresetadapter

--- Back-End/app/members/adapters.py
+++ Back-End/app/members/adapters.py
@@ -4,20 +4,18 @@ from django.conf import settings
 from allauth.account.utils import user_pk_to_url_str
 
 class FrontendResetAdapter(DefaultAccountAdapter):
-    # Method automatically called by Allauth when a user requests a password reset
-    def send_password_reset_mail(self, request, user):
-        # Generates a secure token for reset
-        token = self.token_generator.make_token(user)
-        # Transforms user ID into URL-safe format
-        uid = user_pk_to_url_str(user)
-        # Build the link that will point to your React page.
-        reset_url = f"{settings.FRONTEND_URL}/reset-password/{uid}/{token}"
-        # Context is a Python dictionary containing all the information to inject into the template of the email you send to the user
-        # Prepare the data (context) that the email template will use
-        context = {
-            'user': user,
-            'password_reset_url': reset_url,
-        }
-        # Send the email with the default Allauth template, but using our modified link.
-        self.send_mail('account/email/password_reset', user.email, context)
-
+    # Method automatically called by Allauth when it needs to send an email
+    # template_prefix: The name of the email template to use (for example : account/email/password_reset)
+    # email: The recipient's email address
+    # context: A dictionary with all the variables to be injected into the email (such as uid, token, etc.)
+    def send_mail(self, template_prefix, email, context):
+        print("Mon adapter personnalisé est bien utilisé")
+        if template_prefix == 'account/email/password_reset':
+            user = context.get('user')
+            if user:
+              uid = context.get('uid') or user_pk_to_url_str(user)
+              token = context.get('token')
+              # Replaces the value of context['password_reset_url'] with a URL that points to the frontend.
+              context['password_reset_url'] = f"{settings.FRONTEND_URL}/reset-password/{uid}/{token}"
+        # Sends the email with the updated context
+        super().send_mail(template_prefix, email, context)
