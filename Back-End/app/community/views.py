61e87d3bea13ff6e2333518e764b942f05462158
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-21 03:37:59 +0200

    i add PostViewSet and CommentaryPostViewSet class

---
+++ Back-End/app/community/views.py
@@ -0,0 +1,35 @@
+from rest_framework import viewsets, permissions
+from .models import Post
+from .models import CommentaryPost
+from .serializers import PostSerializer
+from .serializers import CommentaryPostSerializer
+
+# Viewsets uses ModelViewSet, which automatically manages CRUD views
+class PostViewSet(viewsets.ModelViewSet):
+    # Uses PostSerializer to convert objects to JSON and validate the received data
+    serializer_class = PostSerializer
+    # Requires user to be authenticated to access this view
+    permission_classes = [permissions.IsAuthenticated]
+
+    # Everyone can see all posts
+    def get_queryset(self):
+      return Post.objects.all()
+
+    # Force the author field to be the logged in user to prevent the logged in user from choosing the author
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+# Viewsets uses ModelViewSet, which automatically manages CRUD views
+class CommentaryPostViewSet(viewsets.ModelViewSet):
+    # Uses CommentaryPostSerializer to convert objects to JSON and validate the received data
+    serializer_class = CommentaryPostSerializer
+    # Requires user to be authenticated to access this view
+    permission_classes = [permissions.IsAuthenticated]
+
+    # Everyone can see all posts
+    def get_queryset(self):
+      return CommentaryPost.objects.all()
+    
+    # Force the author field to be the logged in user to prevent the logged in user from choosing the author
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
