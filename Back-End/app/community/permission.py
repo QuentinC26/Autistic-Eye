f92da78bbd385a71390b4a7200ac20512d68b1c4
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-21 07:42:56 +0200

    i add one import and the isownerorreadonly class

--- Back-End/app/community/permission.py
+++ Back-End/app/community/permission.py
@@ -1,7 +1,11 @@
 from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    # Automatically called by DRF to check if the user has the right to perform the action on the object (obj)
     def has_object_permission(self, request, view, obj):
+        # Checks if the method used (like GET or HEAD) is a method that does not modify the data.
         if request.method in permissions.SAFE_METHODS:
+            # If the previous condition is true (so the method is "safe"), we grant the permission immediately by returning True.
             return True
+        # Checks that the logged in user (request.user) is the author of the object (obj.author)
         return obj.author == request.user
