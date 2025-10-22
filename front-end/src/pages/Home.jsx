1b1540c7bf21db3aeb672fa7e66b6d94c90c5af5
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-22 17:45:46 +0200

    i replace the basic text for community by the number of post if user connected or deconnected

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,18 +1,60 @@
 import './App.css'
 // Access the data of the context you created
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link, useNavigate } from 'react-router-dom';
 
 function Home() {
   // Get the user from the context
   const { user } = useContext(AuthContext);
+  // List of posts to display
+  const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
+
+  // You tell React what to do when the page is rendered or when the user logs in or out.
+  useEffect(() => {
+    // Retrieves the token stored in the browser
+    const token = localStorage.getItem('accessToken');
+
+    // Basic Url to Retrieves the posts
+    let url = 'http://localhost:8000/api/community/posts/';
+
+    // If the user is logged in, add ?limit=5 to the URL (so 5 posts).
+    // Otherwise, add ?limit=1 (only the most recent).
+    if (user) {
+      url += '?limit=5';
+    } else {
+      url += '?limit=1';
+    }
+
+    // Send a request to the API to retrieve all the comments
+    fetch(url, {
+      // If the user is logged in, the authentication token is added to the header
+      headers: user ? { 
+        'Authorization': 'Token ' + token,
+      } 
+      // If the user is not logged in, nothing is added to the header
+      : {
+      },
+    })
+
+    // You get the response from the API, convert it to JSON, then put the posts in the state to display them.
+    .then(res => res.json())
+    .then(data => {
+      setPosts(data);
+    })
+    // If there is an error in the query, you display it in the console
+    .catch(err => {
+      console.error("Erreur fetch posts", err);
+    });
+  // useEffect restarts if the user changes
+  }, [user]);
 
   const handleRegister = () => {
     // To put the user on the home page
     navigate('/Register_and_login');
   };
+
   return (
     <>
     {/* ? = the condition is True */}
@@ -24,7 +66,18 @@ function Home() {
         <p>Voici les cinq post les plus récents :</p>
         <br />
         <br />
-        <p>POST LES PLUS RECENTS</p>
+        {/* If the posts list is empty, displays a message: No posts to display */}
+        {posts.length === 0 && <p>Aucun post à afficher</p>}
+        {/* Loops through the posts list to process each post */}
+        {posts.map(post => (
+          // Creates a <div> element for each post with a unique key
+          <div key={post.id}>
+            <h4>{post.title}</h4>
+            <h5>{post.subject}</h5>
+            <p>{post.content.substring(0, 100)}...</p>
+            <br />
+          </div>
+        ))}
         <br />
         <br />
         <p>Voici les cinq articles les plus récents :</p>
@@ -35,13 +88,25 @@ function Home() {
         </div>
         ) : (
         <div>
-         <h3>Bienvenue sur Autistic Eye !!</h3>
-         <br />
-         <p>Autistic Eye est un réseau social communautaire qui réunit les personnes autistes et leurs proches. Son but est de combattre l'isolement des personnes autistes et de leur permettre de partager leurs expériences.</p>
-         <br />
-         <p>Les membres d'Autistic Eye ont un accès illimité à la partie Communauté, qui leur permet de partager leurs expériences, d'échanger entre eux et d'obtenir de l'aide. Pour vous donner un aperçu de cette section, voici le post le plus récent ci-dessous :</p>
-         <br />
-         <p>POST LE PLUS RECENT</p>
+        <h3>Bienvenue sur Autistic Eye !!</h3>
+        <br />
+        <p>Autistic Eye est un réseau social communautaire qui réunit les personnes autistes et leurs proches. Son but est de combattre l'isolement des personnes autistes et de leur permettre de partager leurs expériences.</p>
+        <br />
+        <p>Les membres d'Autistic Eye ont un accès illimité à la partie Communauté, qui leur permet de partager leurs expériences, d'échanger entre eux et d'obtenir de l'aide. Pour vous donner un aperçu de cette section, voici le post le plus récent ci-dessous :</p>
+        <br />
+        {/* If the posts list is empty, displays a message: No posts to display */}
+        {posts.length === 0 && <p>Aucun post à afficher</p>}
+        {/* Loops through the posts list to process each post */}
+        {posts.map(post => (
+          // Creates a <div> element for each post with a unique key
+          <div key={post.id}>
+            {/* Displays the post information (title, subject and part of the content) */}
+            <h4>{post.title}</h4>
+            <h5>{post.subject}</h5>
+            <p>{post.content.substring(0, 100)}...</p>
+            <br />
+          </div>
+        ))}
          <br />
          <p>L'autre partie de l'application est la section Article. Dans cette section, les membres d'Autistic Eye ont accès à toutes les ressources externes qui les concernent, comme les démarches administratives, les associations, et bien plus encore.</p>
          <br />
