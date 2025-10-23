d0201043246f198c87e70bf5745dcfaf44554849
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-24 00:24:44 +0200

    i add a infinite while loop with sleep function

---
+++ Back-End/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Infinite loop: executes the following commands over and over again without stopping
+while true; do
+    # Runs the Django command to retrieve the articles and redirects all output (stdout and stderr) to the /app/cron.log file
+    python /app/manage.py fetch_articles >> /app/cron.log 2>&1
+    # Sleep pauses the script for a certain number of seconds, so since 86400 seconds = 24 hours,... 
+    # ...the script waits 24 hours before restarting fetch_articles.
+    sleep 86400 
+# End of while loop
+done
