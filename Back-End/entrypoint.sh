2c313e7f3181b9427a43815238b3c950b32b4ce6
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-24 07:22:58 +0200

    i add a . after loop in the last comment

--- Back-End/entrypoint.sh
+++ Back-End/entrypoint.sh
@@ -7,5 +7,5 @@ while true; do
     # Sleep pauses the script for a certain number of seconds, so since 86400 seconds = 24 hours,... 
     # ...the script waits 24 hours before restarting fetch_articles.
     sleep 86400 
-# End of while loop
+# End of while loop.
 done
