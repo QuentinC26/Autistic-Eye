c264fa2aa00b0468223f1be741f9f4e698dd246f
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-24 14:32:28 +0200

    i add ArticleSerializer class

---
+++ Back-End/app/article/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Article
+
+class ArticleSerializer(serializers.ModelSerializer):
+    # Indicates which template it should use and which fields to include
+    class Meta:
+        # Tells the serializer that the associated model is Article
+        model = Article
+        # Specifies the model fields that you want to include in the serialization
+        fields = ['id', 'title', 'description', 'link', 'publication_date']
