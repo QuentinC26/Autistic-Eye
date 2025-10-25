c291eb5f10159eb2a55233b326f7fe9a3cd7f57c
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-25 22:47:24 +0200

    i replace articles by article

--- Back-End/app/article/urls.py
+++ Back-End/app/article/urls.py
@@ -3,5 +3,5 @@ from .views import ArticleListAPIView
 
 # # Defines existing URL links in the application.
 urlpatterns = [
-  path('api/articles/', ArticleListAPIView.as_view(), name='api_articles'),
+  path('api/article/', ArticleListAPIView.as_view(), name='api_articles'),
 ]
