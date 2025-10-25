a68757388834dee2a704b9e01d5ad0cdb7d1d4ee
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-26 00:53:17 +0200

    i add articlepagination class, one import and pagination_class in the articlelistapiwiew clas

--- Back-End/app/article/views.py
+++ Back-End/app/article/views.py
@@ -1,9 +1,18 @@
 from rest_framework import generics
 from .models import Article
 from .serializers import ArticleSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class ArticlePagination(PageNumberPagination):
+  # Each page will contain a maximum of 10 articles
+  page_size = 10
+
 
 class ArticleListAPIView(generics.ListAPIView):
     # Retrieves all articles and sorts them from newest to oldest
     queryset = Article.objects.all().order_by('-publication_date')
     # Specifies the serializer used to convert Article objects to JSON
     serializer_class = ArticleSerializer
+    # 
+    pagination_class = ArticlePagination
