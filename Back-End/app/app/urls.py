354d1b62566ec46b0d32e27225b3aae396c1ac6b
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-25 22:53:59 +0200

    i replace articles.urls by article.urls

--- Back-End/app/app/urls.py
+++ Back-End/app/app/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('members/', include('members.urls')),
     path('api/community/', include('community.urls')),
-    # Allows you to avoid having the following URL link: api/articles/api/articles
-    path('', include('articles.urls'))
+    # Allows you to avoid having the following URL link: api/article/api/article
+    path('', include('article.urls'))
 ]
