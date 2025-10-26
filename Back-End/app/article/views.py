7ceb6e5a09822bf611fabb248189ad222248d5b5
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-26 16:46:36 +0100

    i add pagination and i adapt the pagination without break the code for displays the last article in the home page

--- Back-End/app/article/views.py
+++ Back-End/app/article/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .models import Article
 from .serializers import ArticleSerializer
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import AllowAny
 
 
 class ArticlePagination(PageNumberPagination):
@@ -10,9 +11,21 @@ class ArticlePagination(PageNumberPagination):
 
 
 class ArticleListAPIView(generics.ListAPIView):
-    # Retrieves all articles and sorts them from newest to oldest
-    queryset = Article.objects.all().order_by('-publication_date')
     # Specifies the serializer used to convert Article objects to JSON
     serializer_class = ArticleSerializer
     # Uses the custom pagination class defined just before
     pagination_class = ArticlePagination
+    # Allows reading without login
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+      # Retrieves all articles and sorts them from newest to oldest
+      queryset = Article.objects.all().order_by('-publication_date')
+      # Check if there is a 'limit' parameter in the URL to limit the number of items returned.
+      limit = self.request.query_params.get('limit')
+
+      # Checks that 'limit' is a number, then limits the queryset to the requested number of items
+      if limit is not None and limit.isdigit():
+          queryset = queryset[:int(limit)]
+      
+      return queryset
