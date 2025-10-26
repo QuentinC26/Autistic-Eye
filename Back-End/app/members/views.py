a6a7c691e7886ad88d865b36e1e8fa2fea9d6f8e
QuentinC26 <q.zuzlewski@gmail.com>
2025-10-26 22:30:33 +0100

    i add return user in perform_create method and i disabled the verify email part

--- Back-End/app/members/views.py
+++ Back-End/app/members/views.py
@@ -22,57 +22,58 @@ class CustomRegisterView(RegisterView):
    # ...after the creation of the new user.
    # perform_create is a name imposed by the Django REST Framework (DRF)
     def perform_create(self, serializer):
-      # Creates and saves the user in the database with the received data
-      user = serializer.save(self.request)
-      # Allows you to say that the account is not yet validated by email and saves this modification
-      user.is_verified = False
-      user.save()
-      # Create a token to verify the user via their email
-      token = generate_email_token(user.email)
-      # Construct a full URL that the user will click in the email with the token as a parameter.
-      verification_link = f"http://localhost:5173/verify-email?token={token}"
+        # Creates and saves the user in the database with the received data
+        user = serializer.save(self.request)
+        # Allows you to say that the account is not yet validated by email and saves this modification
+        user.is_verified = False
+        user.save()
+        # Create a token to verify the user via their email
+        # token = generate_email_token(user.email)
+        # Construct a full URL that the user will click in the email with the token as a parameter.
+        # verification_link = f"http://localhost:5173/verify-email?token={token}"
 
-      send_mail(
+        # send_mail(
         # Subject of the email
-        'Confirmez votre adresse email',
+        # 'Confirmez votre adresse email',
         # Message of the email
-        f'Bonjour {user.first_name},\n\nMerci de vous être inscrit. Cliquez sur ce lien pour vérifier votre adresse email : {verification_link}',
+        # f'Bonjour {user.first_name},\n\nMerci de vous être inscrit. Cliquez sur ce lien pour vérifier votre adresse email : {verification_link}',
         # The one who sends the email
-        settings.DEFAULT_FROM_EMAIL,
+        # settings.DEFAULT_FROM_EMAIL,
         # The one who receives the email
-        [user.email],
+        # [user.email],
         # Know if there is a sending error
-        fail_silently=False,
-        )
+        # fail_silently=False,
+        return user
+        # )
 
 
 # Create a view named VerifyEmailView that inherits from APIView that will respond to http requests
-class VerifyEmailView(APIView):
+# class VerifyEmailView(APIView):
     # Method executed when a GET request is sent to this view
-    def get(self, request):
+    # def get(self, request):
         # Retrieves the token parameter from the URL
-        token = request.GET.get('token')
-        if not token:
-            return Response({"detail": "Token manquant."}, status=status.HTTP_400_BAD_REQUEST)
+        # token = request.GET.get('token')
+        # if not token:
+            # return Response({"detail": "Token manquant."}, status=status.HTTP_400_BAD_REQUEST)
 
-        email = verify_email_token(token)
-        if not email:
-            return Response({"detail": "Token invalide ou expiré."}, status=status.HTTP_400_BAD_REQUEST)
+        # email = verify_email_token(token)
+        # if not email:
+            # return Response({"detail": "Token invalide ou expiré."}, status=status.HTTP_400_BAD_REQUEST)
 
-        try:
+        # try:
             # Search for a user in the database with this email
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            return Response({"detail": "Utilisateur non trouvé."}, status=status.HTTP_404_NOT_FOUND)
+            # user = User.objects.get(email=email)
+        # except User.DoesNotExist:
+            # return Response({"detail": "Utilisateur non trouvé."}, status=status.HTTP_404_NOT_FOUND)
 
-        if user.is_verified:
-            return Response({"detail": "Compte déjà vérifié."})
+        # if user.is_verified:
+            # return Response({"detail": "Compte déjà vérifié."})
         
         # Informs that the account is verified
-        user.is_verified = True
+        # user.is_verified = True
         # Saves the information in the database
-        user.save()
-        return Response({"detail": "Email vérifié avec succès."})
+        # user.save()
+        # return Response({"detail": "Email vérifié avec succès."})
 
 
 # Allows you to reuse all the default login behavior, while customizing it
@@ -85,8 +86,8 @@ class CustomLoginView(LoginView):
         if response.status_code == 200:
             # represents the successfully logged in user.
             user = self.user
-        if not user.is_verified: 
-            return Response( {'detail': 'Merci de vérifier votre email avant de vous connecter.'}, status=status.HTTP_403_FORBIDDEN ) 
+        # if not user.is_verified: 
+            # return Response( {'detail': 'Merci de vérifier votre email avant de vous connecter.'}, status=status.HTTP_403_FORBIDDEN ) 
         return response
 
 
