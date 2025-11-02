16e479d1b8ac2847dc5067c28b5cd7881a86cdc1
QuentinC26 <q.zuzlewski@gmail.com>
2025-11-02 11:22:51 +0100

    i replace all functions article, community ect... by const handleNavagiate = path = navigate(path) line, i modify my return for have a better nav

--- front-end/src/pages/Navigation.jsx
+++ front-end/src/pages/Navigation.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import Autistic_Eye from '../assets/Autistic_Eye.png';
@@ -8,17 +8,9 @@ function Navigation() {
   const { user, logoutUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const handleHome = () => {
-    navigate('/');
-  };
-
-  const handleProfile = () => {
-    navigate('/Profile');
-  };
-
-  const handleRegisterandlogin = () => {
-    navigate('/Register_and_login');
-  };
+  // Allows navigation to another page (for example, the Community page)...
+  // ...this makes the code cleaner by avoiding the need for a navigation function for each page
+  const handleNavigate = (path) => navigate(path);
 
   const handleLogout = () => {
     // for logout the user
@@ -27,40 +19,40 @@ function Navigation() {
     navigate('/');
   };
 
-  const handleArticle = () => {
-    navigate('/Article');
-  };
-
-  const handleCommunity = () => {
-    navigate('/Community');
-  };
-
   return (
-    <nav> 
-      <ul className="ul_list">
-      <li><button onClick={handleHome}>Accueil</button></li>
-      {/* Only a logged in user can view the My Profile page */}
-      {user && (
-        <li><button onClick={handleProfile}>Mon Profil</button></li>
-      )}
-      {!user ? (
-        <li><button onClick={handleRegisterandlogin}>Inscription/Connexion</button></li>
-      ) : (
-        <li><button onClick={handleLogout}>Déconnexion</button></li>
-      )}
-    </ul>
+   <nav className="navbar">
+    {/* Container in the left of the navbar */}
+    <div className="navbar-left">
+        <button onClick={() => handleNavigate('/')}>Accueil</button>
+        {/* If user exists (logged in user), display the My Profile button */}
+        {user && <button onClick={() => handleNavigate('/Profile')}>Mon Profil</button>}
+        {/* Condition to display Registration/Login or Logout depending on whether the user is logged in or not */}
+        {!user ? (
+          <button onClick={() => handleNavigate('/Register_and_login')}>Inscription / Connexion</button>
+        ) : (
+          <button onClick={handleLogout}>Déconnexion</button>
+        )}
+      </div>
     <br></br>
-    <div className="logo-container">
-    <a href="/" target="_blank" rel="noopener noreferrer">
-      <img src={Autistic_Eye} className="logo_of_website" alt="Autistic Eye logo" />
-    </a>
+     {/* Container in the center of the navbar */}
+     <div className="navbar-center">
+        <img
+          src={Autistic_Eye}
+          className="logo_of_website"
+          alt="Autistic Eye logo"
+          // Makes the logo clickable to return to home
+          onClick={() => handleNavigate('/')}
+          // Changes the cursor to indicate that the image is clickable
+          style={{ cursor: 'pointer' }}
+        />
+      </div>
+    <br></br>
+    {/* Container in the right of the navbar */}
+    <div className="navbar-right">
+      <button onClick={() => handleNavigate('/Article')}>Article</button>
+      <button onClick={() => handleNavigate('/Community')}>Communauté</button>
     </div>
-    <br></br>
-    <ul className="ul_list_2">
-    <li><button onClick={handleArticle}>Article</button></li>
-    <li><button onClick={handleCommunity}>Communauté</button></li>
-    </ul>
-    </nav>
+   </nav>
   );
 }
 
