728e5c24f17e08031912ff4d2553c080de09e556
QuentinC26 <q.zuzlewski@gmail.com>
2026-03-08 23:35:46 +0100

    i change the first name by the email in the test member model

--- Back-End/app/members/tests.py
+++ Back-End/app/members/tests.py
@@ -5,5 +5,5 @@ from members.models import User
 # test for verify the model is works well
 def test_member_model():
     member = User.objects.create(first_name="Cindy", last_name="Jackson", age="19", location="New York", email="cindyjackson@gmail.com")
-    assert str(member) == "Cindy"
+    assert str(member) == "cindyjackson@gmail.com"
 
