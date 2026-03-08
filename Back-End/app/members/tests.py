e939936c27f8011cd1b97312fa91657767366830
QuentinC26 <q.zuzlewski@gmail.com>
2026-03-08 22:51:39 +0100

    i modify the import and i add the test for member model

--- Back-End/app/members/tests.py
+++ Back-End/app/members/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import pytest
+from members.models import User
 
-# Create your tests here.
+@pytest.mark.django_db
+# test for verify the model is works well
+def test_member_model():
+    member = User.objects.create(first_name="Cindy", last_name="Jackson", age="19", location="New York", email="cindyjackson@gmail.com")
+    assert str(member) == "Cindy"
+
