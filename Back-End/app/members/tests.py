79d9a33af680b39913f73fbd64d8c123e794a999
QuentinC26 <q.zuzlewski@gmail.com>
2026-03-09 01:45:21 +0100

    i add password in the three tests, the member_login and the delete user test

--- Back-End/app/members/tests.py
+++ Back-End/app/members/tests.py
@@ -5,5 +5,31 @@ from members.models import User
 # test for verify the model is works well
 def test_member_model():
     member = User.objects.create(first_name="Cindy", last_name="Jackson", age="19", location="New York", email="cindyjackson@gmail.com")
+    member.set_password("jesuistropbelle")
+    member.save()
     assert str(member) == "cindyjackson@gmail.com"
 
+@pytest.mark.django_db
+# test for verify the connexion in the application is works well
+def test_member_login(client):
+    member = User.objects.create(first_name="Alain", last_name="Solis", age="29", location="Paris", email="alainsolis@gmail.com")
+    member.set_password("expertcomptabledu29786")
+    member.save()
+    response = client.post(
+        "/members/auth/login/", ({
+            "email": "alainsolis@gmail.com",
+            "password": "expertcomptabledu29786"
+        }),
+        content_type="application/json"
+    )
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+# test for verify the model is works well
+def test_delete_user(client):
+    member = User.objects.create(first_name="Albert", last_name="Dixon", age="36", location="Lille", email="albertdixon@gmail.com")
+    member.set_password("bulbizarredu89")
+    member.save()
+    client.force_login(member)
+    response = client.post("/members/profile/delete/")
+    assert response.status_code == 200
