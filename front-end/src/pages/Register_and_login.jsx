2c3eef96b96afa91ff3516d7cd25d3f91eef0ae1
QuentinC26 <q.zuzlewski@gmail.com>
2026-03-22 15:16:26 +0100

    i replace age by birth_date

--- front-end/src/pages/Register_and_login.jsx
+++ front-end/src/pages/Register_and_login.jsx
@@ -34,7 +34,7 @@ export function Register() {
           password2: data.password,
           first_name: data.first_name,
           last_name: data.last_name,
-          age: data.age,
+          birth_date: data.birth_date,
           location: data.location
       })
       });
@@ -73,9 +73,9 @@ export function Register() {
       {errors.last_name && <span className="error-text">Champ requis</span>}
       </div>
       <div className="form-group">
-      <label className="form-label">*Âge :</label>
-      <input type="number" {...register("age", { required: true })} className="form-input" />
-      {errors.age && <span className="error-text">Champ requis</span>}
+      <label className="form-label">*Date de Naissance :</label>
+      <input type="date" {...register("birth_date", { required: true })} className="form-input" />
+      {errors.birth_date && <span className="error-text">Champ requis</span>}
       </div>
       <div className="form-group">
       <label className="form-label">*Ville :</label>
