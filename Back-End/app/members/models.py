7355db5e4be58493927e68cbea80bae92a56f84c
QuentinC26 <q.zuzlewski@gmail.com>
2026-03-22 13:41:11 +0100

    i add import uuid, i replace age by birth_date and i modify the max_lenght of email

--- Back-End/app/members/models.py
+++ Back-End/app/members/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 
 class User(AbstractUser):
 # If variables are not in the User class, those from the AbstractUser class are taken
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    age = models.IntegerField(default=0)
+    birth_date = models.DateField()
     location = models.CharField(max_length=50)
-    email = models.EmailField(max_length=60, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     
     # for connect in the application, you can write the email
     USERNAME_FIELD = 'email'
