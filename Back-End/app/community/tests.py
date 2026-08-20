f639ffa90f2a7ddb450bfbe3d237e690a4678494
QuentinC26 <q.zuzlewski@gmail.com>
2026-08-20 15:20:43 +0200

    i add the creation of post and deletation of post test

--- Back-End/app/community/tests.py
+++ Back-End/app/community/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+from django.db import models
+from members.models import User
+from community.models import Post
+import pytest
+from rest_framework.test import APIClient
 
-# Create your tests here.
+@pytest.mark.django_db
+# test for check the creation of post is works well
+def test_create_post():
+    client=APIClient()
+    member = User.objects.create(
+        first_name="Christophe",
+        last_name="Lalanne",
+        birth_date="1999-05-01", 
+        location="Lille", 
+        email="christophelalanne@gmail.com"
+    )
+
+    member.set_password("bulbizarredu89")
+    member.save()
+    client.force_authenticate(user=member)
+
+    post = Post.objects.create(
+        author=member,
+        title="Un renouveau",
+        subject="Autre",
+        content="bla bla bla",
+        created_at="12/04/2026"
+    )
+
+    assert post.author == member
+    assert post.title == "Un renouveau"
+
+@pytest.mark.django_db
+# test for check the deletation of post is works well
+def test_delete_post():
+    client=APIClient()
+    member = User.objects.create(
+        first_name="Christophe",
+        last_name="Lalanne",
+        birth_date="1999-05-01", 
+        location="Lille", 
+        email="christophelalanne@gmail.com"
+    )
+
+    member.set_password("bulbizarredu89")
+    member.save()
+    client.force_authenticate(user=member)
+
+    post = Post.objects.create(
+        author=member,
+        title="Un renouveau",
+        subject="Autre",
+        content="bla bla bla",
+        created_at="12/04/2026"
+    )
+
+    response = client.delete(f"/api/community/posts/{post.id}/")
+    assert response.status_code == 204
+
